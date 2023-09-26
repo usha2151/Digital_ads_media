9953019bdcfe9f0d790699196c4a0166b739e12c
BobySwaroop <bobyswaroop555024@gmail.com>
2023-09-26 23:01:58 +0530

    service page done

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -4,7 +4,11 @@ import Google_Product from "./Home/Digital-ads-media-google-product.jpg";
 import Error_404 from "./Home/Digital-ads-media-error-404.jpg";
 import OurAdviser from "./Home/Digital-ads-media-Adviser.png";
 import BestAdviser from "./Home/Digital-ads-media-Best-Adviser.png"
-
+import Affliate_Marketting from "./services/Digital-ads-media-Affliate-marketting.jpg";
+import Mobile_Marketting from "./services/Digital-ads-media-Mobile-Marketting.png";
+import Our_Strategy from "./services/Digital-ads-media-Our-Strategy.jpg";
+import Search_Engine from "./services/Digital-ads-media-Search-engine.jpg";
+import Social_media from "./services/Digital-ads-media-Social-Media.png";
 
 
 
@@ -15,6 +19,11 @@ export {
     Google_Product,
     Error_404,
     OurAdviser,
-    BestAdviser
+    BestAdviser,
+    Affliate_Marketting,
+    Mobile_Marketting,
+    Social_media,
+    Search_Engine,
+    Our_Strategy
 
 }
