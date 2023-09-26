a96cfe5649903d7d86551d8ea608dbc490616156
Usha Singh <ushasing215105@gmail.com>
2023-09-26 20:46:24 +0530

    new updates

--- src/components/pages/Home/Adviser.jsx
+++ src/components/pages/Home/Adviser.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { BestAdviser, OurAdviser, OurAdviserAdviser } from '../../images'
+import { BestAdviser, OurAdviser } from '../../images'
 const Adviser = () => {
   return (
     <>
-      <div className='grid grid-cols-2 container mx-auto' style={{marginTop:'80px'}}>
+    <div className='container mx-auto px-16'>
+      <div className='grid grid-cols-2 gap-4' style={{marginTop:'80px'}}>
         <div>
         <div className="flex items-center justify-start mt-5">
       <p className="px-2 text-sky font-bold text-h6">OUR SKILL</p>
@@ -37,6 +38,7 @@ We’re pride of ourselves on transforming our clients for the better, helping t
        <img src={OurAdviser}></img>
         </div>
 
+      </div>
       </div>
     </>
   )
