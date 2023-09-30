55da9b2bf3d56c5e430ef874d7aa2d86d08c6b7e
BobySwaroop <bobyswaroop555024@gmail.com>
2023-09-30 10:39:00 +0530

    mobile resposnive done

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -4,11 +4,11 @@ import Google_Product from "./Home/Digital-ads-media-google-product.jpg";
 import Error_404 from "./Home/Digital-ads-media-error-404.jpg";
 import OurAdviser from "./Home/Digital-ads-media-Adviser.png";
 import BestAdviser from "./Home/Digital-ads-media-Best-Adviser.png"
-import Affliate_Marketting from "./services/Digital-ads-media-Affliate-marketting.jpg";
-import Mobile_Marketting from "./services/Digital-ads-media-Mobile-Marketting.png";
-import Our_Strategy from "./services/Digital-ads-media-Our-Strategy.jpg";
-import Search_Engine from "./services/Digital-ads-media-Search-engine.jpg";
-import Social_media from "./services/Digital-ads-media-Social-Media.png";
+import Affliate_Marketting from "./services/Affiliate Marketing.jpg";
+import Mobile_Marketting from "./services/ourservicetopimages.jpg";
+import Our_Strategy from "./services/perfomance marketing.jpg";
+import Search_Engine from "./services/top-view-cloud-with-word-seo.jpg";
+import Social_media from "./services/app and web devleopment.jpg";
 import Publisher_bg from "./Publishers/publisher_bg.jpg";
 import Publisher_ab from "./Publishers/publisher_ab.jpg";
 import advertisers_bg from "./Advertisers/advertisers_bg.jpg";
--- src/components/pages/OurServices/OurServices.jsx
+++ src/components/pages/OurServices/OurServices.jsx
@@ -24,10 +24,10 @@ const OurServices = () => {
 
           {/* Buttons */}
           <div className="mt-6">
-            <button className="px-6 py-4 bg-sky font-bold hover:bg-blue text-white mr-4">
+            <button className="px-4 py-3 md:px-6 md:py-4 bg-sky font-bold hover:bg-blue text-white mr-2 md:mr-4">
             <i class="fa-regular fa-circle-right mr-2"></i>Affilate Sign Up
             </button>
-            <button className="px-6 py-4 bg-sky font-bold hover:bg-blue text-white">
+            <button className="px-4 py-3 md:px-6 md:py-4 bg-sky font-bold hover:bg-blue text-white">
             <i class="fa-regular fa-circle-right mr-2"></i>Affilate Login In
             </button>
           </div>
@@ -35,14 +35,14 @@ const OurServices = () => {
       </div>
 
 {/* services card */}
-<div className='bg-dimblue h-96'>
+<div className='bg-dimblue pt-1 pb-24 md:pt-0 md:pb-0 md:p-0 h-3/4 md:h-96'>
 <div className='container mx-auto'>
 
-<div className='grid grid-cols-1 md:grid-cols-5'>
+<div className='grid grid-cols-1 gap-4 md:gap-0 md:grid-cols-5'>
   {ServiceCard.map((data,index)=>
-    <div key={index} className='bg-white text-center mx-auto w-56 p-8 rounded-lg md:relative md:-mt-24'>
+    <div key={index} className='bg-white text-center mx-auto mt-4 w-80 md:w-56 p-2 md:p-8 rounded-lg md:relative md:-mt-24'>
 <span className='font-bold text-h3 text-sky  rounded-md w-16 py-2 mx-auto'>{data.icon}</span>
-<p className='font-bold text-h6  mt-4'>{data.service}</p>
+<p className='font-bold text-h6 mt-2 md:mt-4'>{data.service}</p>
 </div>
   )}
 </div>
